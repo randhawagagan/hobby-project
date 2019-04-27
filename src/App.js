3a0cf34f828d22822c33b464228f9556c418821a
Gagan <randhawa_gagan@live.com>
2019-04-27 22:32:39 +0530

    Props in React practice

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import MainComponent from './components/MainComponent';
+import ContactCard from './components/ContactCard';
 // Set up the React app from scratch
 // Render an App component (defined in a separate file)
 // Inside App, render:
@@ -15,7 +16,20 @@ class App extends Component {
     return (
       <div className="App">
       <NavBar/>
-        <MainComponent/>
+        <div className="contacts">
+        <ContactCard
+           contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
+       />
+       <ContactCard
+           contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
+       />
+       <ContactCard
+           contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
+       />
+       <ContactCard
+           contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
+       />
+       </div>
         <Footer />
       </div>
     );
