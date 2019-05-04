7619beb4cbf11107a30b626a93b8f9e39dc91892
Gagan <randhawa_gagan@live.com>
2019-05-04 12:33:49 +0530

    State game with start over button with bootstrap

--- src/components/StateComponent.js
+++ src/components/StateComponent.js
@@ -6,19 +6,35 @@ class StateComponent extends Component{
     super()
     this.state = { count : 0}
     this.handleClick = this.handleClick.bind(this);
+    this.handleClickClear = this.handleClickClear.bind(this);
   }
   handleClick(){
   this.setState(previousState => {
-    return {
-    count :previousState.count + 1
+    if(previousState.count < 10){
+      return {
+      count :previousState.count + 1
+    }
+    }
+  else{
+    return alert("Dont you have something better to do!");
   }
+  } )
+  }
+  handleClickClear(){
+  this.setState(previousState => {
+      return {
+      count :0
+    }
   } )
   }
 render(){
   return (
        <div>
            <h1>{this.state.count}</h1>
-           <button onClick={this.handleClick}>Click me to Play!</button>
+           <h2><button className="btn btn-primary" onClick={this.handleClick}>Click me to Play!</button>
+           <br/>
+           <button className="btn btn-danger" onClick={this.handleClickClear}>Start Over!</button>
+</h2>
        </div>
    )
 }
